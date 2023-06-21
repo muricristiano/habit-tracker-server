64353b0f4cd2394ba9de4930cac01d6fe76c8f85
Murilo Cristiano <murilocristiano@MURI-MACBOOK.station>
2023-06-21 22:52:15 +0200

    update

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,8 @@ app.register(cors) // Without config, everything can access the backend app
 app.register(appRoutes)
 
 app.listen({
-    port: 3333
+    port: 3333,
+    host: '0.0.0.0' //Fastify by default doesn't allow connections from IP. Only localhost device (Not working Expo Go External Devices)
 }).then(() => {
     console.log('HTTP server listening on port 3333...')
 })
